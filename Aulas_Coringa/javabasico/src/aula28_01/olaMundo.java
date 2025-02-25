184a2e666bf61c833ee098fc68ea3d0fcabdc9d6
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-02-25 11:14:16 -0300

    Add files via upload

---
+++ Aulas_Coringa/javabasico/src/aula28_01/olaMundo.java
@@ -0,0 +1,23 @@
+package aula28_01;
+
+public class olaMundo {
+
+	public static void main(String[] args) {
+		
+		// Usamos o "//" para criar um comentário...
+		
+		// Saída
+		// Escrevendo "Syso" e clicando em "ctrl" + "Espaço" você cria essa linha de código.
+		
+		
+		// O comando "println" imprime as mensagens em linhas individuais.
+		System.out.println("Olá, Mundo!"); // Instrução para a saída de dados.
+		System.out.println("Primeira linha.");
+		System.out.println("Segunda linha.");
+
+		// O Comando "print" imprimi as mensagens na mesma linha
+		System.out.print("Esses textos");
+		System.out.print(" ficam juntos.");
+	}
+
+}
