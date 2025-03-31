072841c968f5a8a85df756ab34513368d072581c
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2025-03-31 10:57:59 -0300

    Add files via upload

---
+++ AulaSete/src/AulaSete/vetor.java
@@ -0,0 +1,19 @@
+package AulaSete;
+
+public class vetor {
+	public static void main(String[] args) {
+
+		int vetor[] = { 1, 2, 3 };
+		for (int i = vetor.length-1; i >= 0; i--) {
+			System.out.println(vetor[i]);
+		}
+
+//		for (int i : vetor) {
+//			System.out.println(vetor[i]+" ");
+//		}
+		
+		
+		
+		
+	}
+}
